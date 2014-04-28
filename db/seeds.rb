c79966409020a6dd39167cb5f71a1ddd291b81fc
tigarcia <tim.m.garcia@gmail.com>
2014-04-28 15:22:12 -0700

    Fixing views to use brand instead of measurement

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ r3 = Recipe.create(:name => 'French Onion Soup', :course => 'Apps', :cooktime =>
 
 i1 = Ingredient.create(:name => 'Flour', :brand => 'Gold Medal', :image => 'http://3.bp.blogspot.com/_Iel3IXJ_g7A/RzIFHOZyqRI/AAAAAAAACvk/f6zlHQ_QFSA/s400/flour.jpg')
 i2 = Ingredient.create(:name => 'Eggs', :brand => 'Clover', :image => 'http://images.elephantjournal.com/wp-content/uploads/2010/04/eggs1.jpg')
-i3 = Ingredient.create(:name => 'Sugar', :brand => 'C&H', :image => 'http://verifiedafricanmango.com/wp-content/uploads/2012/08/Sugar.jpg')
+i3 = Ingredient.create(:name => 'Sugar', :brand => 'C&H', :image => 'http://i.walmartimages.com/i/p/00/01/58/00/03/0001580003061_500X500.jpg')
 i4 = Ingredient.create(:name => 'Milk', :brand => 'Clover', :image => 'http://images.wisegeek.com/pitcher-of-milk.jpg')
 i5 = Ingredient.create(:name => 'Butter', :brand => 'Land-O-Lakes', :image => 'http://www.cheesemaking.com/images/recipes/35Butter/Pics/pic51.jpg')
 i6 = Ingredient.create(:name => 'Bacon', :brand => 'Kirkland Signature',:image => 'http://4.bp.blogspot.com/-HX7l3B-dPXY/UN0FeGidWHI/AAAAAAAADZE/FF9svNR700I/s1600/crispy-bacon.jpg')

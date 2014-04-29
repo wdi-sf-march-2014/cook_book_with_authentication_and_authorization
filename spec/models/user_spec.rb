ad75f29482ad5b5ce5007aa6d2303946aad2a996
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-04-29 13:39:51 -0700

    refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,18 +5,18 @@ describe User do
   describe 'email' do
 
     it 'should have an email' do
-      user = User.create(name: "jackie", password: "aaaaaa", password_confirmation: "aaaaaa")
+      user = FactoryGirl.build(:user, email: "")
       user.should_not be_valid
     end
 
     it 'should be in email format' do
-      user = User.create(name: "jackie", email: "fake_email",password: "aaaaaa", password_confirmation: "aaaaaa")
+      user = FactoryGirl.build(:user, email: "fake_email")
       user.should_not be_valid
     end
 
     it 'should be unique' do
-      user_1 = User.create(name: "jackie", email: "jackie@jackie.com",password: "aaaaaa", password_confirmation: "aaaaaa")
-      user_2 = User.create(name: "jackie", email: "jackie@jackie.com",password: "aaaaaa", password_confirmation: "aaaaaa")
+      user_1 = FactoryGirl.create(:user, email: "jackie@jackie.com")
+      user_2 = FactoryGirl.build(:user, email: "jackie@jackie.com")
       user_2.should_not be_valid
     end
   end
